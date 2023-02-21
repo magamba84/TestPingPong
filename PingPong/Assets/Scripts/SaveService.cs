35a1ec3063afcec02af0c221a7f8aac7c615458e
magamba <magamba@list.ru>
2023-02-21 19:48:24 +0400

    removed unused debugs

--- PingPong/Assets/Scripts/SaveService.cs
+++ PingPong/Assets/Scripts/SaveService.cs
@@ -19,13 +19,12 @@ public class SaveService : MonoBehaviour
 
 	public void SaveProgress(string dataJson)
 	{
-		//Debug.Log($"SatelliteDataSaver_file: save progress to {Application.persistentDataPath}/{projId}.txt \n saved data: {dataJson}");
 		FilesUtility.SaveTextFile(dataJson);
 	}
 
 	public string LoadProgress()
 	{
-		//Debug.Log($"SatelliteDataSaver_file: load progress from {Application.persistentDataPath}/{projId}.txt");
+		
 		return FilesUtility.LoadTextFile();
 	}
 }
